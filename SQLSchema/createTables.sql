83471bf7aaf1c3d18b155d60ec7357cf53239b3f
landroskill2 <landroskill2@gmail.com>
2022-10-12 17:03:25 +0100

    continuation of bd

--- SQLSchema/createTables.sql
+++ SQLSchema/createTables.sql
@@ -1,24 +1,29 @@
 	
 CREATE TABLE FUNCIONARIO(
-		Nome varchar(200),
-		DtNascimento date,
+		Nome varchar(200) NOT NULL,
+		DtNascimento date NOT NULL,
 		Telemovel varchar(15),
-		Pais varchar(8),
-		TipoDocIdent varchar(30),
-		DocIdent varchar(15),
-		ValidadeDocIdent date,
-		CatProf enum ('', ' '),
-		NIF varchar(15),
-		NISS varchar(15),
-		Morada varchar(200),
-		ContratoInicio date,
+		Naturalidade varchar(20),
+		Pais varchar(8) NOT NULL,
+		TipoDocIdent varchar(30) NOT NULL,
+		DocIdent varchar(15) NOT NULL,
+		ValidadeDocIdent date NOT NULL,
+		CatProf varchar(20) references CATEGORIAS_PROFISSIONAIS(codigo) NOT NULL,
+		NIF varchar(15), /* PRIMARY KEY*/
+		NISS varchar(15) NOT NULL,
+		Morada varchar(200) NOT NULL,
+		ContratoInicio date ,
 		ContratoFim date,
-		VencimentoBase double,
-		TipoSalario	enum ('', ' '),
-		SalarioReal	double,
-		Calcado	double,
-		CartaConducao boolean,
-		IBAN varchar(20)
+		VencimentoBase float(5, 2) NOT NULL,
+		TipoSalario	varchar NOT NULL,
+		SalarioReal	float(5, 2) NOT NULL,
+		Calcado	float(2,1),
+		CartaConducao varchar(3),
+		IBAN varchar(20),
+		Obra varchar(20) references OBRA(CodigoInterno),
+
+		constraint chk_TipoSalario CHECK(TipoSalario IN("horario", "fixo")),
+		constraint chk_CartaConducao CHECK(CartaConducao IN("Sim", "Nao"))
 );
 
 CREATE TABLE OBRA (
@@ -28,29 +33,36 @@ CREATE TABLE OBRA (
         DataInicio date,
         DataFim date,
         Pais varchar(8),
-        Funcionarios ???,
-        AutosDeMedicao ???,
-        FolhasDePonto ???
+        AutosDeMedicao varchar(100),
 );
 
-CREATE TABLE  (
-		Funcionario ???,
-        CodigoObra varchar(20),
+CREATE TABLE OBRAS_PASSADAS(
+		Funcionario varchar(15) references Funcionario(NIF),
+		Obra varchar(20) references Obra(CodigoInterno)
+)
+
+CREATE TABLE DIA_TRABALHO(
+		Funcionario varchar(15) references Funcionario(NIF),
+        CodigoObra varchar(20)references Obra(CodigoInterno),
         Dia int,
 		Horas int,
-        Mes enum ('janeiro', 'fevereiro', 
-        'marco', 'abril', 'maio', 'junho', 'julho', 'agosto', 
-        'setembro', 'outubro', 'novembro', 'dezembro'),
+        Mes varchar(9), 
         Ano int,
-        Valor double,
-        foreign key (CodigoObra) references OBRA(CodigoInterno),
-        foreign key () references FUNCIONARIO()
+        Valor float(7,2),
+		constraint chk_Mes CHECK(Mes IN('janeiro', 'fevereiro', 
+        'marco', 'abril', 'maio', 'junho', 'julho', 'agosto', 
+        'setembro', 'outubro', 'novembro', 'dezembro')),
 );
 
-CREATE TABLE  (
-		Funcionario ???,
-        Mes enum ('janeiro', 'fevereiro', 
-        'marco', 'abril', 'maio', 'junho', 'julho', 'agosto', 
-        'setembro', 'outubro', 'novembro', 'dezembro'),
-        Valor double
+
+CREATE TABLE CATEGORIAS_PROFISSIONAIS(
+		codigo varchar(20) PRIMARY KEY,
+		nomenclatura varchar(50)
+);
+
+CREATE TABLE SALARIO_FINAL(
+		Funcionario varchar(15) references Funcionario(NIF),
+		Mes varchar(9), 
+        Ano int,
+        Valor float(7,2),
 );
