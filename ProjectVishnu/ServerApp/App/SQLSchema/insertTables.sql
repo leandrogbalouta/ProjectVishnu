2df3312e22a1ec8775d6b17f0029fe2a331450c7
Fouto99 <franciscomartins99@gmail.com>
2022-11-14 19:15:12 +0000

    insertTables updated

--- ProjectVishnu/ServerApp/App/SQLSchema/insertTables.sql
+++ ProjectVishnu/ServerApp/App/SQLSchema/insertTables.sql
@@ -3,6 +3,12 @@ INSERT INTO CATEGORIAS_PROFISSIONAIS
 ('1234567','XXXXXXX'),
 ('1243123','XXXXXXX');
 
+INSERT INTO MERCADO
+(mercado,sigla,dia_inicio,dia_fim) values
+('portugal','PT',21,20),
+('espanha','ES',26,25),
+('franca','FR',21,20);
+
 INSERT INTO FUNCIONARIO 
 (nome,dtnascimento,telemovel,contactoemergencia,nacionalidade,mercado,
 tipodocident,docident,tituloresidencia,manifestacaointeresse,
@@ -32,7 +38,15 @@ cartaconducao,iban) values
 '12436587091','Rua Umberto Delgado n66',
 '2019-08-20','2022-12-31',900.0,
 'horario',900.0,null,'Sim',
-'22222222222222222222');
+'22222222222222222222'),
+('Andre Ferro','1999-03-04',
+'914324322','214455444','Espanhola',
+'espanha','CC','123321111',null,null,
+'2025-03-03','1243123','321311322',
+'12333221222','Rua Joao Andrade 520',
+'2021-05-06','2023-05-06',900.0,
+'horario',900.0,null,'Nao',
+'12312232132113211133');
 
 INSERT INTO OBRA
 (codigointerno,designacao,cliente,
@@ -42,9 +56,58 @@ datainicio,datafim,mercado,autosdemedicao) values
 'Autos de medicao'),
 ('OB22PT01','Obra de um edificio','ISEL',
 '2022-05-09','2025-05-09','portugal',
+'Autos de medicao'),
+('OB21PT01','Obra de uma escola','ESEL',
+'2021-06-30','2024-06-30','portugal',
+'Autos de medicao'),
+('OB22ES02','Obra de um estadio','Real Madrid',
+'2022-07-09','2026-09-08','espanha',
+'Autos de medicao'),
+('OB22FR01','Obra de um estadio','PSG',
+'2022-07-09','2026-09-08','franca',
 'Autos de medicao');
 
 INSERT INTO FUNCIONARIOS_OBRAS
 (funcionario,obra,datacomeco,datafim) values
-('255896379','OB22PT01','2022-05-10',
-null);
+('234567899','OB22PT01','2022-05-21',null);
+
+INSERT INTO DIA_TRABALHO
+(funcionario,codigoobra,dia,horas,mes,ano,valor) values
+('234567899','OB22PT01',21,8,'junho',2022,56),
+('234567899','OB22PT01',22,8,'junho',2022,56),
+('234567899','OB22PT01',23,8,'junho',2022,56),
+('234567899','OB22PT01',24,8,'junho',2022,56),
+('234567899','OB22PT01',25,8,'junho',2022,56),
+('234567899','OB22PT01',26,8,'junho',2022,56),
+('234567899','OB22PT01',27,8,'junho',2022,56),
+('234567899','OB22PT01',28,8,'junho',2022,56),
+('234567899','OB22PT01',29,8,'junho',2022,56),
+('234567899','OB22PT01',30,8,'junho',2022,56),
+('234567899','OB22PT01',1,8,'junho',2022,56),
+('234567899','OB22PT01',2,8,'junho',2022,56),
+('234567899','OB22PT01',3,8,'junho',2022,56),
+('234567899','OB22PT01',4,8,'junho',2022,56),
+('234567899','OB22PT01',5,8,'junho',2022,56),
+('234567899','OB22PT01',6,8,'junho',2022,56),
+('234567899','OB22PT01',7,8,'junho',2022,56),
+('234567899','OB22PT01',8,8,'junho',2022,56),
+('234567899','OB22PT01',9,8,'junho',2022,56),
+('234567899','OB22PT01',10,8,'junho',2022,56),
+('234567899','OB22PT01',11,8,'junho',2022,56),
+('234567899','OB22PT01',12,8,'junho',2022,56),
+('234567899','OB22PT01',13,8,'junho',2022,56),
+('234567899','OB22PT01',14,8,'junho',2022,56),
+('234567899','OB22PT01',15,8,'junho',2022,56),
+('234567899','OB22PT01',16,8,'junho',2022,56),
+('234567899','OB22PT01',17,8,'junho',2022,56),
+('234567899','OB22PT01',18,8,'junho',2022,56),
+('234567899','OB22PT01',19,8,'junho',2022,56),
+('234567899','OB22PT01',20,8,'junho',2022,56);
+
+INSERT INTO SALARIO_FINAL
+(funcionario,mes,ano,valorfinal,valorapagar) values
+('234567899','junho',2022,999,999);
+
+INSERT INTO CONTA
+(username,pwd) values
+('afonso','afonso123');
