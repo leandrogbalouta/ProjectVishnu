aaf70cd94154309bf7c5b9331614fc57aeef3368
landroskill2 <landroskill2@gmail.com>
2022-12-15 22:25:11 +0000

    useffect added to funcionarioCreation

--- ProjectVishnu/ClientApp/src/pages/FuncionarioCreation.js
+++ ProjectVishnu/ClientApp/src/pages/FuncionarioCreation.js
@@ -1,9 +1,9 @@
 import { useNavigate } from "react-router-dom"
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 
 export function FuncionarioCreation() {
-  const inputs = document.querySelectorAll("input")
+
     const [funcionario, setFuncionario] = useState({
       nome: '',
       dtnascimento: '',
@@ -31,11 +31,17 @@ export function FuncionarioCreation() {
     })
     const navigate = useNavigate()
 
-    inputs.forEach(element => {
-      element.addEventListener('change', (event)=>{
-        setFuncionario({ element : event.target.value})
+
+    useEffect(() => {
+      const inputs = document.querySelectorAll("input")
+      
+      inputs.forEach(element => {
+        element.addEventListener('change', (event)=>{
+          setFuncionario({ element : event.target.value})
+        })
       })
-    })
+    }, [])
+    
 
 
     async function AddFuncionario(){
@@ -57,104 +63,104 @@ export function FuncionarioCreation() {
         </thead>
         <tbody>
           
-            <div>
+            <tr>
               <label>Nome</label>
               <input type={"text"} className="funcionarioInput" id="nome" name="nome" value={funcionario.nome} required/>
-            </div>        
-            <div>
+            </tr>        
+            <tr>
               <label>Data de Nascimento</label>
               <input type={"date"} name="dtnascimento" required />
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>Telemóvel</label>
               <input type={"text"} name="telemovel"required />
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>Contacto de emergência</label>
               <input type={"text"} name="contactoemergencia" required />
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>Nacionalidade</label>
               <input type={"text"} name="nacionalidade" required />
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>Mercado</label>
               <input type={"text"} name="mercado" required />
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>Tipo de documento de identificação</label>
               <input type={"text"} name="tipodocident" required />
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>Número do documento de identificação</label>
               <input type={"text"} name="docident" required />
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>Título de Residência</label>
               <input type={"text"} name="tituloresidencia" />
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>Manifestação de interesse</label>
               <input type={"text"} name="manifestacaointeresse" />
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>Validade do documento de identificação</label>
               <input type={"date"} name="validadedocident" required />
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>Categoria Profissional</label>
               <input type={"text"} name="catprof" required />
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>Número de Identificação Fiscal (NIF)</label>
               <input type={"text"} name="nif" required />
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>Número de Identificação de Segurança Social (NISS)</label>
               <input type={"text"} name="niss" required />
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>Morada</label>
               <input type={"text"} name="morada" required />
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>Data de ínicio de contrato</label>
               <input type={"date"} name="contratoinicio" required />
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>Data de fim de contrato</label>
               <input type={"date"} name="contratofim" required />
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>Vencimento base</label>
               <input type={"number"} name="vencimentobase" required />
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>Tipo de salário</label>
               <input type={"radio"} name="tiposalario" value="horario" />
               <label for="horario">Horário</label>
               <input type={"radio"} name="tiposalario" value="fixo" />
               <label for="fixo">Fixo</label>
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>Salário Real</label>
               <input type={"number"} name="salarioreal" required />
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>Calçado</label>
               <input type={"number"} name="calcado" required />
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>Carta de condução</label>
               <input type={"radio"} name="cartaconducao" value="Sim" />
               <label for="Sim">Sim</label>
               <input type={"radio"} name="cartaconducao" value="Nao" />
               <label for="Nao">Não</label>
-            </div>
-            <div>
+            </tr>
+            <tr>
               <label>IBAN</label>
               <input type={"text"} name="iban" required />
-            </div>
+            </tr>
           <button type="button" class="btn btn-primary" onClick={() => AddFuncionario()}>Criar</button>
           
         </tbody>
