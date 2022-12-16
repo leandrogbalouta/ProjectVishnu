9134e3f62279c93bbc142766ff84161fca75fc3f
landroskill2 <landroskill2@gmail.com>
2022-12-16 19:01:35 +0000

    criar button working  added navbar to funcionariocreation

--- ProjectVishnu/ClientApp/src/pages/FuncionarioCreation.js
+++ ProjectVishnu/ClientApp/src/pages/FuncionarioCreation.js
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom"
 import React, {useEffect, useState} from 'react';
+import { FilterBar } from "../components/FilterBar";
+import { Layout } from "../components/Layout";
 
 
 export function FuncionarioCreation() {
@@ -67,115 +69,119 @@ export function FuncionarioCreation() {
     
 
     return(
-      <form>
-      <table className='table table-striped' aria-labelledby="tabelLabel">
-        <thead>
-          <b>Criar um Funcionário</b>
-        </thead>
-        <tbody>
-          
-            <tr>
-              <label>Nome</label>
-              <input type={"text"} className="funcionarioInput" id="nome" name="nome" onChange={inputs} required/>
-            </tr>        
-            {/* <tr>
-              <label>Data de Nascimento</label>
-              <input type={"date"} name="dtnascimento" required />
-            </tr>
-            <tr>
-              <label>Telemóvel</label>
-              <input type={"text"} name="telemovel"required />
-            </tr>
-            <tr>
-              <label>Contacto de emergência</label>
-              <input type={"text"} name="contactoemergencia" required />
-            </tr>
-            <tr>
-              <label>Nacionalidade</label>
-              <input type={"text"} name="nacionalidade" required />
-            </tr>
-            <tr>
-              <label>Mercado</label>
-              <input type={"text"} name="mercado" required />
-            </tr>
-            <tr>
-              <label>Tipo de documento de identificação</label>
-              <input type={"text"} name="tipodocident" required />
-            </tr>
-            <tr>
-              <label>Número do documento de identificação</label>
-              <input type={"text"} name="docident" required />
-            </tr>
-            <tr>
-              <label>Título de Residência</label>
-              <input type={"text"} name="tituloresidencia" />
-            </tr>
-            <tr>
-              <label>Manifestação de interesse</label>
-              <input type={"text"} name="manifestacaointeresse" />
-            </tr>
-            <tr>
-              <label>Validade do documento de identificação</label>
-              <input type={"date"} name="validadedocident" required />
-            </tr>
-            <tr>
-              <label>Categoria Profissional</label>
-              <input type={"text"} name="catprof" required />
-            </tr>
-            <tr>
-              <label>Número de Identificação Fiscal (NIF)</label>
-              <input type={"text"} name="nif" required />
-            </tr>
-            <tr>
-              <label>Número de Identificação de Segurança Social (NISS)</label>
-              <input type={"text"} name="niss" required />
-            </tr>
-            <tr>
-              <label>Morada</label>
-              <input type={"text"} name="morada" required />
-            </tr>
-            <tr>
-              <label>Data de ínicio de contrato</label>
-              <input type={"date"} name="contratoinicio" required />
-            </tr>
-            <tr>
-              <label>Data de fim de contrato</label>
-              <input type={"date"} name="contratofim" required />
-            </tr>
-            <tr>
-              <label>Vencimento base</label>
-              <input type={"number"} name="vencimentobase" required />
-            </tr>
-            <tr>
-              <label>Tipo de salário</label>
-              <input type={"radio"} name="tiposalario" value="horario" />
-              <label for="horario">Horário</label>
-              <input type={"radio"} name="tiposalario" value="fixo" />
-              <label for="fixo">Fixo</label>
-            </tr>
-            <tr>
-              <label>Salário Real</label>
-              <input type={"number"} name="salarioreal" required />
-            </tr>
-            <tr>
-              <label>Calçado</label>
-              <input type={"number"} name="calcado" required />
-            </tr>
-            <tr>
-              <label>Carta de condução</label>
-              <input type={"radio"} name="cartaconducao" value="Sim" />
-              <label for="Sim">Sim</label>
-              <input type={"radio"} name="cartaconducao" value="Nao" />
-              <label for="Nao">Não</label>
-            </tr>
-            <tr>
-              <label>IBAN</label>
-              <input type={"text"} name="iban" required />
-            </tr> */}
-          <button type="button" class="btn btn-primary" onClick={() => AddFuncionario()}>Criar</button>
-          
-        </tbody>
-      </table> 
-      </form>
+      <Layout>
+        <div>
+          <form>
+          <table className='table table-striped' aria-labelledby="tabelLabel">
+            <thead>
+              <b>Criar um Funcionário</b>
+            </thead>
+            <tbody>
+              
+                <tr>
+                  <label>Nome</label>
+                  <input type={"text"} className="funcionarioInput" id="nome" name="nome" onChange={inputs} required/>
+                </tr>        
+                {/* <tr>
+                  <label>Data de Nascimento</label>
+                  <input type={"date"} name="dtnascimento" required />
+                </tr>
+                <tr>
+                  <label>Telemóvel</label>
+                  <input type={"text"} name="telemovel"required />
+                </tr>
+                <tr>
+                  <label>Contacto de emergência</label>
+                  <input type={"text"} name="contactoemergencia" required />
+                </tr>
+                <tr>
+                  <label>Nacionalidade</label>
+                  <input type={"text"} name="nacionalidade" required />
+                </tr>
+                <tr>
+                  <label>Mercado</label>
+                  <input type={"text"} name="mercado" required />
+                </tr>
+                <tr>
+                  <label>Tipo de documento de identificação</label>
+                  <input type={"text"} name="tipodocident" required />
+                </tr>
+                <tr>
+                  <label>Número do documento de identificação</label>
+                  <input type={"text"} name="docident" required />
+                </tr>
+                <tr>
+                  <label>Título de Residência</label>
+                  <input type={"text"} name="tituloresidencia" />
+                </tr>
+                <tr>
+                  <label>Manifestação de interesse</label>
+                  <input type={"text"} name="manifestacaointeresse" />
+                </tr>
+                <tr>
+                  <label>Validade do documento de identificação</label>
+                  <input type={"date"} name="validadedocident" required />
+                </tr>
+                <tr>
+                  <label>Categoria Profissional</label>
+                  <input type={"text"} name="catprof" required />
+                </tr>
+                <tr>
+                  <label>Número de Identificação Fiscal (NIF)</label>
+                  <input type={"text"} name="nif" required />
+                </tr>
+                <tr>
+                  <label>Número de Identificação de Segurança Social (NISS)</label>
+                  <input type={"text"} name="niss" required />
+                </tr>
+                <tr>
+                  <label>Morada</label>
+                  <input type={"text"} name="morada" required />
+                </tr>
+                <tr>
+                  <label>Data de ínicio de contrato</label>
+                  <input type={"date"} name="contratoinicio" required />
+                </tr>
+                <tr>
+                  <label>Data de fim de contrato</label>
+                  <input type={"date"} name="contratofim" required />
+                </tr>
+                <tr>
+                  <label>Vencimento base</label>
+                  <input type={"number"} name="vencimentobase" required />
+                </tr>
+                <tr>
+                  <label>Tipo de salário</label>
+                  <input type={"radio"} name="tiposalario" value="horario" />
+                  <label for="horario">Horário</label>
+                  <input type={"radio"} name="tiposalario" value="fixo" />
+                  <label for="fixo">Fixo</label>
+                </tr>
+                <tr>
+                  <label>Salário Real</label>
+                  <input type={"number"} name="salarioreal" required />
+                </tr>
+                <tr>
+                  <label>Calçado</label>
+                  <input type={"number"} name="calcado" required />
+                </tr>
+                <tr>
+                  <label>Carta de condução</label>
+                  <input type={"radio"} name="cartaconducao" value="Sim" />
+                  <label for="Sim">Sim</label>
+                  <input type={"radio"} name="cartaconducao" value="Nao" />
+                  <label for="Nao">Não</label>
+                </tr>
+                <tr>
+                  <label>IBAN</label>
+                  <input type={"text"} name="iban" required />
+                </tr> */}
+              <button type="button" class="btn btn-primary" onClick={() => AddFuncionario()}>Criar</button>
+              
+            </tbody>
+          </table> 
+          </form>
+        </div>
+      </Layout>
     )
 }
