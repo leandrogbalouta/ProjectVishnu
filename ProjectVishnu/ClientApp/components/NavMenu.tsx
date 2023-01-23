807152ad80f1dcc239516875827f9dca7f14db99
Luis Martins <luismwolf@gmail.com>
2023-01-23 10:27:26 +0000

    navmenu minor responsive changes.

--- ProjectVishnu/ClientApp/components/NavMenu.tsx
+++ ProjectVishnu/ClientApp/components/NavMenu.tsx
@@ -62,9 +62,9 @@ export default function NavMenu() {
   return (
     <header className="sticky top-0 !z-[1000] shadow-sm shadow-slate-600">
       <nav className="bg-slate-900 border-gray-200 px-2 sm:px-4 py-2.5 rounded">
-        <div className="container-fluid flex flex-wrap items-center justify-between mx-0">
+        <div className="container-fluid flex flex-wrap items-center">
           <span
-            className="self-center text-xl font-semibold whitespace-nowrap !text-white cursor-pointer"
+            className="self-center text-xl font-semibold whitespace-nowrap !text-white cursor-pointer mr-auto"
             onClick={() => router.push("/")}
           >
             ProjectVishnu
@@ -73,7 +73,7 @@ export default function NavMenu() {
             <button
               id="theme-toggle"
               type="button"
-              className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+              className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
             >
               <BsFillSunFill
                 id="theme-toggle-dark-icon"
@@ -92,6 +92,7 @@ export default function NavMenu() {
               <span className="sr-only">Open main menu</span>
               <RiMenu4Line className="text-2xl sm:text-3xl text-orange-400" />
             </button>
+          </div>
             <div
               className={toggleClass + " w-full md:block md:w-auto"}
               id="navbar-default"
@@ -119,7 +120,6 @@ export default function NavMenu() {
                 </li>
               </ul>
             </div>
-          </div>
         </div>
       </nav>
     </header>
