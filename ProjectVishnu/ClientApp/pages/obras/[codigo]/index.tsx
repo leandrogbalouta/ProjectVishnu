85c343439afe2ed556771ba064d17b0c38f3cc59
Luis Martins <luismwolf@gmail.com>
2023-02-02 21:32:27 +0000

    adicionado info obra no index de obra

--- ProjectVishnu/ClientApp/pages/obras/[codigo]/index.tsx
+++ ProjectVishnu/ClientApp/pages/obras/[codigo]/index.tsx
@@ -1,12 +1,26 @@
 import React, { ChangeEvent, useEffect, useState } from "react";
-import { fetchObra, createFolhaDePonto, fetchFolhaDePontoAllByobra as fetchFolhaDePontoAllByObra } from "../../../common/APICalls";
-import { Button, Input, Spinner, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import {
+  fetchObra,
+  createFolhaDePonto,
+  fetchFolhaDePontoAllByobra as fetchFolhaDePontoAllByObra,
+  fetchMercados,
+} from "../../../common/APICalls";
+import {
+  Button,
+  Input,
+  Spinner,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import IObraOutput from "../../../common/Interfaces/Obra/IObraOutput";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import IFolhaDePontoOutput from "../../../common/Interfaces/FolhaDePonto/IFolhaDePontoOutput";
-import FolhaDePontoObra from './folha-de-ponto/[data]';
-import IFolhaDePontoInfoModel from '../../../common/Interfaces/FolhaDePonto/IFolhaDePontoInfoModel';
-
+import FolhaDePontoObra from "./folha-de-ponto/[data]";
+import IFolhaDePontoInfoModel from "../../../common/Interfaces/FolhaDePonto/IFolhaDePontoInfoModel";
 
 //TODO: ADICIONAR LISTA DE FOLHAS DE PONTO JÁ CRIADAS
 export default function Obra() {
@@ -23,7 +37,7 @@ export default function Obra() {
     setData(event!.target!.value!);
   };
 
-  let contents = obra === null ? <Spinner/> : renderObra(obra, folhasDePonto);
+  let contents = obra === null ? <Spinner /> : renderObra(obra, folhasDePonto);
 
   // check this
   async function submitFolhaDePonto() {
@@ -38,11 +52,16 @@ export default function Obra() {
     const result = array.pop();
     console.log(result);
     // double check this workin
-    router.push({pathname : result!, query : {info : JSON.stringify(respData)} }, result!);
+    router.push(
+      { pathname: result!, query: { info: JSON.stringify(respData) } },
+      result!
+    );
   }
 
-  async function redirectToFolhaDePonto(folhaDePonto : any) {
-    router.push(`/obras/${codigo}/folha-de-ponto/${folhaDePonto.ano}-${folhaDePonto.mes}`);
+  async function redirectToFolhaDePonto(folhaDePonto: any) {
+    router.push(
+      `/obras/${codigo}/folha-de-ponto/${folhaDePonto.ano}-${folhaDePonto.mes}`
+    );
   }
 
   useEffect(() => {
@@ -58,38 +77,63 @@ export default function Obra() {
     };
     populateObraData();
     populateFolhasDePontoData();
-  }, []);
+  }, [codigo]);
 
-  return <div>
-    {contents}
-    </div>;
+  return <div className="flex h-full w-full">{contents}</div>;
 
-  function renderObra(obra: IObraOutput, folhasDePonto : any) {
+  function renderObra(obra: IObraOutput, folhasDePonto: any) {
     return (
-      <div>
-        <Table className="" aria-labelledby="tabelLabel">
-          <Thead>
-            <Tr className="data-table-header">
-              <Th>Mes</Th>
-              <Th>Ano</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {/* TODO check folhasdeponto type */}
-            {folhasDePonto && folhasDePonto.map((folhaDePonto: IFolhaDePontoInfoModel) => (
-              <Tr
-                className="data-table-row"
-                key={`${folhaDePonto.ano}${folhaDePonto.mes}`}
-                onClick={() =>
-                  redirectToFolhaDePonto(folhaDePonto)
-                }
-              >
-                <Td>{folhaDePonto.mes}</Td>
-                <Td>{folhaDePonto.ano}</Td>
+      <div className="flex flex-col h-full w-full">
+        <div className="p-3 mb-3 bg-slate-800 text-cyan-100 rounded-xl">
+          <p className="text-xl font-bold ml-3">Detalhes de obra:</p>
+          <div className="flex justify-between m-3 flex-wrap gap-3">
+            <div>
+              <p className="obra-heading">Código interno:</p>
+              <p>{obra.codigoInterno}</p>
+            </div>
+            <div>
+              <p className="obra-heading">Designação</p>
+              <p>{obra.designacao}</p>
+            </div>
+            <div>
+              <p className="obra-heading">Cliente:</p>
+              <p>{obra.cliente}</p>
+            </div>
+            <div>
+              <p className="obra-heading">Data de inicio:</p>              
+              <p>{obra.datainicio}</p>
+            </div>
+            <div>
+              <p className="obra-heading">Mercado:</p>
+              <p className="capitalize">{obra.mercado}</p>
+            </div>
+            </div>
+
+        </div>
+        <div id="table-container" className="flex-1 overflow-scroll">
+          <Table className="overflow-scroll" aria-labelledby="tabelLabel">
+            <Thead>
+              <Tr className="data-table-header">
+                <Th>Mes</Th>
+                <Th>Ano</Th>
               </Tr>
-            ))}
-          </Tbody>
-        </Table>
+            </Thead>
+            <Tbody>
+              {/* TODO check folhasdeponto type */}
+              {folhasDePonto &&
+                folhasDePonto.map((folhaDePonto: IFolhaDePontoInfoModel) => (
+                  <Tr
+                    className="data-table-row"
+                    key={`${folhaDePonto.ano}${folhaDePonto.mes}`}
+                    onClick={() => redirectToFolhaDePonto(folhaDePonto)}
+                  >
+                    <Td>{folhaDePonto.mes}</Td>
+                    <Td>{folhaDePonto.ano}</Td>
+                  </Tr>
+                ))}
+            </Tbody>
+          </Table>
+        </div>
         <div id="create-folha-de-ponto-container">
           <label htmlFor="date">Data de início:</label>
           <div id="create-folha-de-ponto-inner-container" className="flex">
