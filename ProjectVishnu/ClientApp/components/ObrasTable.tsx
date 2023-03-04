c2b84302bb55c1906e940584abf8d9298cac1f01
Luis Martins <luismwolf@gmail.com>
2023-03-04 20:36:39 +0000

    adicionada coluna de estado a ObraTable

--- ProjectVishnu/ClientApp/components/ObrasTable.tsx
+++ ProjectVishnu/ClientApp/components/ObrasTable.tsx
@@ -3,10 +3,32 @@ import { AddFuncionarioToObra } from "../common/APICalls";
 import IObraOutput from "../common/Interfaces/Obra/IObraOutput";
 
 interface Props {
-    obras: IObraOutput[];
-    dataOnRowClick?: (codigoInterno: string) => void;
-  }
+  obras: IObraOutput[];
+  dataOnRowClick?: (codigoInterno: string) => void;
+}
 export default function ObrasTable({ obras, dataOnRowClick }: Props) {
+console.log();
+  function TdState({ state }: { state: string }) {
+    let estado = '';
+    let classe = '';
+    switch (state) {
+      default:
+        break;
+      case "por-comecar":
+        classe = "bg-slate-100";
+        estado = "Por começar";
+        break;
+      case "completada":
+        classe = "bg-emerald-300";
+        estado = "Completada";
+        break;
+      case "em-curso":
+        classe = "bg-cyan-300";
+        estado = "Em curso";
+        break;
+    }
+    return <Td className={"text-black ".concat(classe)}>{estado}</Td>
+  }
   return (
     <div id="table-container" className="overflow-x-scroll flex-1">
       <Table className="table table-striped" aria-labelledby="tabelLabel">
@@ -16,19 +38,23 @@ export default function ObrasTable({ obras, dataOnRowClick }: Props) {
             <Th>Designação</Th>
             <Th>Cliente</Th>
             <Th>Mercado</Th>
+            <Th>Estado</Th>
           </Tr>
         </Thead>
         <Tbody>
           {obras.map((obra) => (
             <Tr
               className="data-table-row"
-              onClick={() => dataOnRowClick && dataOnRowClick(obra.codigoInterno)}
+              onClick={() =>
+                dataOnRowClick && dataOnRowClick(obra.codigoInterno)
+              }
               key={obra.codigoInterno}
             >
               <Td>{obra.codigoInterno}</Td>
               <Td>{obra.designacao}</Td>
               <Td>{obra.cliente}</Td>
               <Td className="capitalize">{obra.mercado}</Td>
+              <TdState state={obra.estado} />
             </Tr>
           ))}
         </Tbody>
